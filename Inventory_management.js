1f46a9ed666bf965b3b9655ef9b468fb265a88b4
Leslie Rodriguez <leslie286@usf.edu>
2024-09-22 01:13:48 -0400

    Initialize inventory with product objects

--- Inventory_management.js
+++ Inventory_management.js
@@ -4,7 +4,7 @@ let Inventory = [
     {name: 'Expresso machine', Price:3000, Quantity: 6}
     {name: 'Refrigerator' , Price: 2000, Quantity: 2}
     {name: 'Microwave', Price: 100, Quantity: 2}
-    {name: 'Coffee Beans', Price: 20, Quantity: 300 }
+    {name: 'Coffee Beans', Price: 20, Quantity: 350 }
 
         
 ];
