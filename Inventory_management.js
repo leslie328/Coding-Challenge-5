f46ae15ed4d7a277fa04fb77c3344de6aad31056
Leslie Rodriguez <leslie286@usf.edu>
2024-09-22 12:17:43 -0400

    Initialize orders array

--- Inventory_management.js
+++ Inventory_management.js
@@ -4,12 +4,29 @@ let Inventory = [
     {name: 'Expresso machine', Price:3000, Quantity: 6}
     {name: 'Refrigerator' , Price: 2000, Quantity: 2}
     {name: 'Microwave', Price: 100, Quantity: 2}
-    {name: 'Coffee Beans', Price: 20, Quantity: 30 }
+    {name: 'Coffee Beans', Price: 20, Quantity: 350}
 
         
 ];
 
 // Task 2: Create an Orders Array of Order Objects
+    let array =[];
 
+orders.push({
+    Customer:'Bryan Ross'
+    Products: [
+        {Name:'Cappucino', Quantity: 2}
+        {Name:'latte', Quantity: 1}
+    ]
+    Status: 'complete'
+});
+
+orders.push ({
+    Customer: Alison Mae'
+    Products: [
+    {Name: 'Iced Vanilla Latte', Quantity: 4}
+    {Name: 'Mocha', Quantity: 2}
 ]
-}
+Status: 'Pending'
+})
+
