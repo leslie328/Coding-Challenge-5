28563fbfdd7d05aba32d5ab31c1ee92c4760adbd
Leslie Rodriguez <leslie286@usf.edu>
2024-09-22 01:08:42 -0400

    Initialize inventory with product objects

---
+++ Inventory_management.js
@@ -0,0 +1,13 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+let Inventory = [
+    {name: 'Expresso machine', Price:3000, Quantity: 6}
+    {name: 'Refrigerator' , Price: 2000, Quantity: 2}
+    {name: 'Microwave', Price: 100, Quantity: 2}
+    {name: 'Coffee Beans', Price: 20, Quantity: 300 }
+
+        
+];
+
+]
+}
