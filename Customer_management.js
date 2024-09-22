a5db0107786ce0ee1820877fd05a8a70e562cc2a
Leslie Rodriguez <leslie286@usf.edu>
2024-09-22 12:46:41 -0400

    Initialize inventory with product objects

--- Customer_management.js
+++ Customer_management.js
@@ -1,10 +1,10 @@
 // Task 1: Create an Inventory Array of Product Objects
 
 let Inventory = [
-    {name: 'Expresso machine', Price:3000, Quantity: 6}
-    {name: 'Refrigerator' , Price: 2000, Quantity: 2}
-    {name: 'Microwave', Price: 100, Quantity: 2}
-    {name: 'Coffee Beans', Price: 20, Quantity: 350}
+    {name: 'Expresso', Price:6, Quantity: 40}
+    {name: 'Mocha' , Price: 8, Quantity: 10}
+    {name: 'Latte', Price: 7, Quantity: 30}
+    {name: 'Coffee Beans', Price: 10, Quantity: 20}
 
         
 ];
